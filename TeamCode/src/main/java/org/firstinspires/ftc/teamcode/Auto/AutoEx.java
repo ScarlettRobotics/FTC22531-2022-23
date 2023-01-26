599900efbfd6e7d674e190660c4730256dd88009
Temirlan Nurmambetov <cdefrst58@gmail.com>
2023-01-26 14:53:09 -0700

    Beeepppepee
    TEstitn testing beep booop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoEx.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-public class AutoEx {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous
+public class AutoEx extends OpMode {
+
+    public void init(){
+        WebcamName webcam = hardwareMap.get(WebcamName.class, "Webcam");// creating an openCvWebcam instance
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam);// then using camera factory to create a camera instance using the webcam
+
+        // Connects to the camera device
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                camera.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
+                camera.setPipeline(Pipeline);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+    }
+
+    public void loop(){}
 }
