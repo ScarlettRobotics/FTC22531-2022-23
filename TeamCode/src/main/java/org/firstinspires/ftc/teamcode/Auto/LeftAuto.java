b5041df5b23d401ec727f16e3ef6cbef5cd97fae
Scarlett Robotics <team4733@gmail.com>
2023-01-27 16:39:49 -0700

    DANIEL EDIT Add new Pipeline filterer and add measurement to move robot by
    self explanatory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LeftAuto.java
@@ -1,5 +1,15 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
+/** MOVEMENTS TO MAKE:
+ * adjust to high junction
+ * strafe 7 in
+ * move 52 in forward
+ * strafe 12 in
+ * CASES:
+ *  1: strafe 12 in/36 in
+ *  2: strafe 12 in
+ *  3: strafe 36 in/12 in */
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name="Left Auto", group="Robot")
