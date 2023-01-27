b52aba0c250efa8a3a8482bf244b4db3700500c4
Scarlett Robotics <team4733@gmail.com>
2023-01-27 15:48:33 -0700

    DANIEL EDIT Optimize/neatify code in Core folder
    Here's a list of what was done to make the code more optimized and cleaner:
     - ClawCore.java
       - Removed null initializations, as they're redundant
       - Removed the 'claw' keyword in methods (eg. clawToggle() -> toggle() )
     - SlideCore.java
       - Removed sections that attempted to map buttons to junction heights (they don't work)
       - Added some comments
     - TriMotorDrive.java
       - Added some comments
       - Removed 'largest' variable in setPowers(), as it's always 1
       - Removed setCenterPower(), and moved it to setPowers()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/ClawSlideManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/ClawSlideManager.java
@@ -28,38 +28,12 @@ public abstract class ClawSlideManager extends OpMode {
         // controllerNum determines the gamepad that controls the robot
         switch (controllerNum) {
             case 1:
-                // Set to specific set heights
-                if (gamepad1.a) slide.moveToJunction("GROUND");
-                if (gamepad1.x) slide.moveToJunction("LOW");
-                if (gamepad1.y) slide.moveToJunction("MEDIUM");
-                if (gamepad1.b) slide.moveToJunction("HIGH");
-                // rushed code
                 if (gamepad1.dpad_up) slide.linearAdjustHeight(-30);
                 if (gamepad1.dpad_down) slide.linearAdjustHeight(30);
-                // Move by a cone height
-                // If pgamepad isn't included, the code in the conditional will run every frame instead of once on a button press.
-                /*if (gamepad1.dpad_up && !pgamepad_dpad_up) slide.adjustHeight(1);
-                if (gamepad1.dpad_down && !pgamepad_dpad_down) slide.adjustHeight(-1);
-                // Update previous gamepad presses
-                pgamepad_dpad_up = gamepad1.dpad_up;
-                pgamepad_dpad_down = gamepad1.dpad_down;*/
                 break;
             case 2:
-                // Set to specific set heights
-                if (gamepad2.a) slide.moveToJunction("GROUND");
-                if (gamepad2.x) slide.moveToJunction("LOW");
-                if (gamepad2.y) slide.moveToJunction("MEDIUM");
-                if (gamepad2.b) slide.moveToJunction("HIGH");
-                // rushed code
                 if (gamepad2.dpad_up) slide.linearAdjustHeight(-30);
                 if (gamepad2.dpad_down) slide.linearAdjustHeight(30);
-                // Move by a cone height.
-                // If pgamepad isn't included, the code in the conditional will run every frame instead of once on a button press.
-                /*if (gamepad2.dpad_up && !pgamepad_dpad_up) slide.adjustHeight(1);
-                if (gamepad2.dpad_down && !pgamepad_dpad_down) slide.adjustHeight(-1);
-                // Update previous gamepad presses
-                pgamepad_dpad_up = gamepad2.dpad_up;
-                pgamepad_dpad_down = gamepad2.dpad_down;*/
                 break;
         }
         slide.update();
@@ -73,16 +47,16 @@ public abstract class ClawSlideManager extends OpMode {
             case 1:
                 // Open/close claw if A/B is pressed (respectively)
                 if (gamepad1.left_bumper) {
-                    claw.clawOpen();
+                    claw.open();
                 } else if (gamepad1.right_bumper) {
-                    claw.clawClose();
+                    claw.close();
                 }
             case 2:
                 // Open/close claw if A/B is pressed (respectively)
                 if (gamepad2.left_bumper) {
-                    claw.clawOpen();
+                    claw.open();
                 } else if (gamepad2.right_bumper) {
-                    claw.clawClose();
+                    claw.close();
                 }
         }
         claw.telemetry(telemetry);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/SlideCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/SlideCore.java
@@ -11,43 +11,16 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class SlideCore {
     // Initialize DC motor variable
     protected final DcMotor slideMotor;
-    // Height of each junction. This array is ordered ground -> low -> medium -> high junction.
-    private final int[] junctionHeights = {-510, -2400, -4200, -5100}; //TODO ADJUST VALUES
-    // The goal position for the slide to move to
-    protected int goalPosition = junctionHeights[0];
+    // Position that the slide wants to be at
+    private int goalPosition;
 
     // Map DC motor variable to driver hub
     public SlideCore (HardwareMap hardwareMap) {
         slideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
+        goalPosition = slideMotor.getCurrentPosition();
     }
 
-
-    /* Moves the slide to the height of a junction. Input options for junctionType are ground, low, medium, and high.*/
-    public void moveToJunction(final String junctionType) {
-        switch(junctionType.toUpperCase()) {
-            case "GROUND": //move to ground in arena
-                goalPosition = junctionHeights[0];
-                return;
-            case "LOW": //move to low junction
-                goalPosition = junctionHeights[1];
-                return;
-            case "MEDIUM": //move to medium junction
-                goalPosition = junctionHeights[2];
-                return;
-            case "HIGH": //move to high junction
-                goalPosition = junctionHeights[3];
-        }
-    }
-
-    /* Changes the height of the slide up by 'conesMoved' cones.
-     * If conesMoves is negative, the slide moves down by the specified cone amount. */
-    public void adjustHeight(final int conesMoved) {
-        // Height that needs to be moved to move to the next cone in a cone stack
-        //TODO ADJUST VALUE
-        final int coneHeight = -260;
-        goalPosition += (coneHeight*conesMoved);
-    }
-
+    /* Moves the slide by the given amount */
     public void linearAdjustHeight(final int amount) {
         goalPosition += amount;
     }
