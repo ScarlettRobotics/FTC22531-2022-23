8d27c5aa6101ddbdefe3ee68370ca5c5cf85289e
Scarlett Robotics <team4733@gmail.com>
2023-01-28 12:19:25 -0700

    DANIEL EDIT Found right value for encoder value -> 1 rotation
    yay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/MotorDistanceFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/MotorDistanceFinder.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Drive;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.Core.ClawSlideManager;
 
 /** Used to find the conversion value for encoder values -> rotation
  * conversion value for rotation -> cm will be done with maths */
+@TeleOp(name = "MotorDistanceFinder", group = "auto")
 public class MotorDistanceFinder extends ClawSlideManager {
     private int goalPosition = 0;
     @Override
@@ -12,6 +14,6 @@ public class MotorDistanceFinder extends ClawSlideManager {
         drive.leftMotor.setTargetPosition(goalPosition);
         drive.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         drive.leftMotor.setPower(1);
-        if (gamepad1.a) goalPosition = 260;
+        if (gamepad1.a) goalPosition = 1400;
     }
 }
