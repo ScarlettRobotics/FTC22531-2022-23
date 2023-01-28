0d8731c5ed5542df5e56b234907f5d632afab979
Temirlan Nurmambetov <cdefrst58@gmail.com>
2023-01-27 21:20:45 -0700

    Created OpenCv
    creates the webcam object and sets the pipline to it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/OpenCv.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Core;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Core.Pipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+
+@Autonomous
+class OpenCv extends OpMode {
+    OpenCvWebcam webcam;
+
+    @Override
+    public void init(){
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+        webcam.setPipeline(new Pipeline());
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+    }
+
+    @Override
+    public void start() {
+        webcam.stopStreaming();
+    }
+
+
+    public void loop(){
+
+    }
+
+}
