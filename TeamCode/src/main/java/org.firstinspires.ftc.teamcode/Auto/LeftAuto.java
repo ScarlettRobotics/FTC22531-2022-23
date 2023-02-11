3243c8073b97c7b6e25de75659e43b95b36107ab
Scarlett Robotics <team4733@gmail.com>
2023-02-11 15:47:42 -0700

    Create untested auto movement
    Implemented class in AutoEventHandler
    Hasn't been tested yet
    
    Co-Authored-By: Daniel Liang <74940473+someone2060@users.noreply.github.com>

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/LeftAuto.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/LeftAuto.java
@@ -28,6 +28,7 @@ public class LeftAuto extends LinearOpMode {
     protected WebcamCore webcam;
     protected CameraServoCore cameraServo;
 
+    protected AutoEventHandler autoEventHandler;
     protected SleeveDetector sleeveDetector;
 
     @Override
@@ -42,68 +43,95 @@ public class LeftAuto extends LinearOpMode {
         sleeveDetector.updateSleevePos();
 
         // TODO rightMotor moves before leftMotor; they should move at the same time
-        //drive.moveInches(40, 40, 0);
         // strafe right to center on tile
 
         // run until the end of match (driver pressed STOP)
         while(opModeIsActive()) {
-            if ((int)(runtime.time() / 600) % 2 == 1) {
+            // Centre, and close claw
+            if (autoEventHandler.actionOccurred(0, runtime.time())) {
+                drive.moveInches(0, 0, 2);
                 claw.close();
-            } else {
-                claw.open();
             }
 
-            //TODO
+            // Move forward 2 tiles, and open claw to highest height
+            if (autoEventHandler.actionOccurred(1, runtime.time())) {
+                drive.moveInches(100, 100, 30);
+            }
+
+            // Move to highest position
+            if (3000 < runtime.time() && runtime.time() <= 6000) {
+                slide.slideUp();
+            }
+
+            // Centre with pole
+            if (autoEventHandler.actionOccurred(2, runtime.time())) {
+                drive.moveInches(0, 0, 11);
+            }
+
+            // Move slightly forward
+            if (autoEventHandler.actionOccurred(3, runtime.time())) {
+                drive.moveInches(5, 5, 0);
+            }
+
+            // Slightly move slide down
+            if (8000 < runtime.time() && runtime.time() <= 8300) {
+                slide.slideDown();
+            }
+
+            // Drop cone on slide
+            if (autoEventHandler.actionOccurred(4, runtime.time())) {
+                claw.open();
+                drive.moveInches(-5, -5, 0);
+            }
+
+            // Park to correct position
+            if (autoEventHandler.actionOccurred(5, runtime.time())) {
+                if (sleeveDetector.getSleevePos() == 1) {
+                    drive.moveInches(0, 0, -33);
+                } else if (sleeveDetector.getSleevePos() == 3) {
+                    drive.moveInches(0, 0, 11);
+                } else {
+                    drive.moveInches(0, 0, -11);
+                }
+            }
+
+            // Clap while waiting
+            if (runtime.time() >= 10000) {
+                if ((int)(runtime.time() / 600) % 2 == 1) {
+                    claw.open();
+                } else {
+                    claw.close();
+                }
+            }
+
             drive.update();
             addTelemetry(telemetry);
         }
-        //proceed forward
-
-        //raise arm
-
-        //turn right to post
-
-        //forward to post
-
-        //slide down slightly
-
-        claw.open(); // To drop cone
-
-        // Raise claw
-
-        //Reverse
-
-        //Lower slide
-
-
-        //PARK
-
-        //switch state based on cam
-        //park 1
-
-        //park 2
-
-        //park 3
-
-        //fin - burn rest of time standing still
-
-
     }
 
     /** Runs everything related to class setup */
     private void initialize() {
-        // Define classes
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //init motor/servo classes
         drive = new TriMotorDrive(hardwareMap);
         claw = new ClawCore(hardwareMap);
         slide = new SlideCore(hardwareMap);
-
+        //init webcam classes
         webcam = new WebcamCore(hardwareMap);
         cameraServo = new CameraServoCore(hardwareMap);
-
+        //init auto classes
+        autoEventHandler = new AutoEventHandler();
         sleeveDetector = new SleeveDetector();
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        // Add times where the robot takes actions
+        autoEventHandler.addDetectionTime(0);
+        autoEventHandler.addDetectionTime(1000);
+        autoEventHandler.addDetectionTime(6000);
+        autoEventHandler.addDetectionTime(7000);
+        autoEventHandler.addDetectionTime(8300);
+        autoEventHandler.addDetectionTime(10000);
     }
 
     private void addTelemetry(Telemetry telemetry) {
