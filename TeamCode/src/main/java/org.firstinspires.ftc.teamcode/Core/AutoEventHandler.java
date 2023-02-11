0942652c899093318e3e7472ca3f96330b403f1b
Scarlett Robotics <team4733@gmail.com>
2023-02-11 15:05:42 -0700

    Create AutoEventHandler.java
    read documentation, and improve if able to do so
    
    Co-Authored-By: Daniel Liang <74940473+someone2060@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Core/AutoEventHandler.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Core;
+
+import org.firstinspires.ftc.robotcore.internal.android.dx.util.IntList;
+import java.util.ArrayList;
+
+/*** Handler for determining when a specific time in LinearOpMode has passed.
+ * Times to check for passing are stored in timeToCheck.
+ * previouslyRan corresponds to each index item in timesToCheck.
+ * Once a time in timesToCheck has passed, the corresponding index in previouslyRan will move to true.
+ * Because of this, this class will return true the first time ElapsedTime passes the timer,
+ *  and false after subsequent checks,
+ * This class should be managed in these steps:
+ *  1. Run addDetectionTime() to save a time to check if code has ran.
+ *  2. In the opModeIsActive() loop, TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO*/
+public class AutoEventHandler {
+    private IntList timesToCheck;
+    private ArrayList<Boolean> previouslyRan;
+
+    /** TODO */
+    public void addDetectionTime(int millis) {
+        timesToCheck.add(millis);
+        previouslyRan.add(false);
+    }
+
+    /** TODO */
+    public boolean actionOccurred(int index, int timeMillis) {
+        if (previouslyRan.get(index)) {
+            return false;
+        }
+        if (timesToCheck.get(index) > timeMillis) {
+            return false;
+        }
+        previouslyRan.set(index, true);
+        return true;
+    }
+}
